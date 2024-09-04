da21570b0f77e27b4d7e0ae5e1f5d7222bcfe0a3
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2024-09-04 20:12:46 +0200

    Add XCode app basics

---
+++ F1 Data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  F1 Data
+//
+//  Created by Florian DAVID on 04/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ F1 Data/F1_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_DataApp.swift
+//  F1 Data
+//
+//  Created by Florian DAVID on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct F1_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
