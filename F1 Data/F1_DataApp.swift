7199b02ff0cd85ea21056885b7feb5a7499c6d50
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2024-10-01 22:02:22 +0200

    Update project structure

--- F1 Data/F1_DataApp.swift
+++ F1 Data/F1_DataApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct F1_DataApp: App {
+    init() {
+        setFontsForBars()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(AppViewModel())
         }
+    }
+    
+    func setFontsForBars() {
+        let attributesBold: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Formula1-Display-Bold", size: 18)!,
+            .textEffect: NSAttributedString.TextEffectStyle.letterpressStyle,
+        ]
+        
+        let attributesReg: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Formula1-Display-Regular", size: 14)!,
+            .textEffect: NSAttributedString.TextEffectStyle.letterpressStyle
+        ]
+        
+        UITabBarItem.appearance().setTitleTextAttributes(attributesReg, for: .normal)
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributesReg, for: .normal)
+        UINavigationBar.appearance().titleTextAttributes = attributesBold
+        
+        UISegmentedControl.appearance().setTitleTextAttributes(attributesReg, for: .normal)
+        UISegmentedControl.appearance().backgroundColor = UIColor(Color.gray)
     }
 }
